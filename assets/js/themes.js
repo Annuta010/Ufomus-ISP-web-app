175243e22d357fb43c5ce1a74313bb18c2846c64
Annuta010 <57099295+Annuta010@users.noreply.github.com>
2021-07-07 10:49:03 +0300

    Update themes.js

--- assets/js/themes.js
+++ assets/js/themes.js
@@ -1,30 +1,53 @@
-let themeBtn = document.getElementById('theme-btn');
+
+let themeBt = document.querySelector('button.theme-btn');
+let themeLi = document.querySelector('p.theme-btn');
 let logoL = document.querySelector(".logo-light");
 let logoD = document.querySelector(".logo-dark");
 
 if(localStorage.getItem('theme') !== null) {
     document.querySelector('body').dataset.theme = 'dark';
     document.querySelector('body').classList.add('dark');
-    themeBtn.textContent = "Светлая тема";
+        if(themeBt) {
+                themeBt.textContent = "Светлая тема";
+        }
+        if(themeLi) {
+            themeLi.textContent = "Светлая тема";
+        }
     logoD.classList.add('hidden');
     logoL.classList.remove('hidden');
 }
 
-themeBtn.onclick = function() {
+
+themeLi.addEventListener("click", function() {
     document.querySelector('body').classList.toggle('dark');
     if(document.querySelector('body').classList.contains('dark')) {
         document.querySelector('body').dataset.theme = 'dark';
-        themeBtn.textContent = "Светлая тема";
+        themeLi.textContent = "Светлая тема";
         localStorage.setItem('theme', 'dark');
         logoD.classList.add('hidden');
         logoL.classList.remove('hidden');
     } else {
         document.querySelector('body').dataset.theme = "";
         localStorage.removeItem('theme', 'dark');
-        themeBtn.textContent = "Темная тема";
+        themeLi.textContent = "Темная тема";
         logoD.classList.remove('hidden');
         logoL.classList.add('hidden');
     }
+}); 
     
-    
-}
+themeBt.addEventListener("click", function() {
+    document.querySelector('body').classList.toggle('dark');
+    if(document.querySelector('body').classList.contains('dark')) {
+        document.querySelector('body').dataset.theme = 'dark';
+        themeBt.textContent = "Светлая тема";
+        localStorage.setItem('theme', 'dark');
+        logoD.classList.add('hidden');
+        logoL.classList.remove('hidden');
+    } else {
+        document.querySelector('body').dataset.theme = "";
+        localStorage.removeItem('theme', 'dark');
+        themeBt.textContent = "Темная тема";
+        logoD.classList.remove('hidden');
+        logoL.classList.add('hidden');
+    }
+});
